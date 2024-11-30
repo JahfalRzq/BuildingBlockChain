024a9ac9061c6276c2ffdb5396963e4ecbcab2c3
JahfalRzq <jahfalrizqi14@gmail.com>
2024-12-01 04:20:17 +0700

    fixing transaction test file and transaction file

---
+++ wallet/transaction.js
@@ -0,0 +1,19 @@
+const { v4: uuidv4 } = require('uuid');
+
+class Transaction {
+    constructor({ senderWallet, recipient, amount }) {
+        this.id = uuidv4();
+        this.outputMap = this.createOutputMap({ senderWallet, recipient, amount });
+    }
+
+    createOutputMap({ senderWallet, recipient, amount }) {
+        const outputMap = {};
+
+        outputMap[recipient] = amount;
+        outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
+
+        return outputMap;
+    }
+}
+
+module.exports = Transaction;
--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,33 +1,32 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 
-describe('Transaction', () =>{
+describe('Transaction', () => {
     let transaction, senderWallet, recipient, amount;
-    
-    beforeEach(() =>{
+
+    beforeEach(() => {
         senderWallet = new Wallet();
         recipient = 'recipient-publicKey';
         amount = 50;
-        transaction = new Transaction({senderWallet, recipient, amount})
+        transaction = new Transaction({ senderWallet, recipient, amount });
     });
 
-    it('has an `id`', () =>{
-        expect(transaction).toHavePropperty('id');
+    it('has an `id`', () => {
+        expect(transaction).toHaveProperty('id');
     });
 
-    describe('outputMap', () =>{
-        it('has `outputMap`',() =>{
-            expect(transaction).toHavePropperty('outputMap');
+    describe('outputMap', () => {
+        it('has `outputMap`', () => {
+            expect(transaction).toHaveProperty('outputMap');
         });
 
-        it('output the amount to the recipient',() =>{
+        it('outputs the amount to the recipient', () => {
             expect(transaction.outputMap[recipient]).toEqual(amount);
         });
 
-        it('outputs the remaining balance for the `senderWallet`',() =>{
+        it('outputs the remaining balance for the `senderWallet`', () => {
             expect(transaction.outputMap[senderWallet.publicKey])
                 .toEqual(senderWallet.balance - amount);
         });
     });
-
 });
