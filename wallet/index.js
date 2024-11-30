58a4a440137a3f7e710410fd598bb115769b8404
JahfalRzq <jahfalrizqi14@gmail.com>
2024-11-30 19:29:58 +0700

    Key Pair & Public Key addres using eliptic 'secp256k1'

--- wallet/index.js
+++ wallet/index.js
@@ -1,8 +1,14 @@
-const {STARTING_BALLANCE} = require('../config')
+const {STARTING_BALLANCE} = require('../config');
+const {ec} = require('../util');
+
 
 class Wallet {
     constructor(){
         this.balance = STARTING_BALLANCE;
+
+        const keyPair = ec.genKeyPair();
+
+        this.publicKey = keyPair.getPublic();
     }
 }
 
