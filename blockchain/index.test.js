716de54aafcbed7258ece79f5ebf8d020c2ff77f
JahfalRzq <jahfalrizqi14@gmail.com>
2024-12-01 05:43:20 +0700

    cohesive utility

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -1,6 +1,6 @@
 const Blockchain = require('.');
 const Block = require('./block');
-const cryptoHash = require('../util/crypto-hash');
+const {cryptoHash} = require('../util');
 const hexToBinary = require('hex-to-binary');
 
 
