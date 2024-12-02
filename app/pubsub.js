715b2ccf6301c2d5cfe66e56ca6b2832012529e1
JahfalRzq <jahfalrizqi14@gmail.com>
2024-12-02 13:34:46 +0700

    broadcast the transaction

--- app/pubsub.js
+++ app/pubsub.js
@@ -2,12 +2,14 @@ const redis = require('redis');
 
 const CHANNELS = {
     TEST: 'TEST',
-    BLOCKCHAIN: 'BLOCKCHAIN'
+    BLOCKCHAIN: 'BLOCKCHAIN',
+    TRANSACTION :'TRANSACTION'
 };
 
 class PubSub {
-    constructor({ blockchain }) {
+    constructor({ blockchain,transactionPool }) {
         this.blockchain = blockchain;
+        this.transactionPool = transactionPool;
 
         this.publisher = redis.createClient();
         this.subscriber = redis.createClient();
@@ -27,9 +29,17 @@ class PubSub {
 
         const parsedMessage = JSON.parse(message);
 
-        if (channel === CHANNELS.BLOCKCHAIN) {
-            this.blockchain.replaceChain(parsedMessage);
+        switch(channel) {
+            case CHANNELS.BLOCKCHAIN :
+                this.blockchain.replaceChain(parsedMessage);
+                break;
+            case CHANNELS.TRANSACTION :
+                this.transactionPool.setTransaction(parsedMessage);
+                break;
+            default :
+            return;
         }
+
     }
 
     subscribeToChannels() {
@@ -57,6 +67,13 @@ class PubSub {
             channel: CHANNELS.BLOCKCHAIN,
             message: JSON.stringify(this.blockchain.chain)
         });
+    }
+
+    broadcastTransaction() {
+        this.publish({
+            channel : CHANNELS.TRANSACTION,
+            message : JSON.stringify(transaction)
+        })
     }
 }
 
