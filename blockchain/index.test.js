7c8b0916b1eea092a73f8261ff935b2edd651b0f
JahfalRzq <jahfalrizqi14@gmail.com>
2024-12-06 00:24:25 +0700

    create testing for valid transaction data on blockchain

--- blockchain/index.test.js
+++ blockchain/index.test.js
@@ -144,4 +144,42 @@ describe('Blockchain', () => {
           });
         });
       });
+      describe('validTransactionData()', () => {
+        let transaction, rewardTransaction, wallet;
+        beforeEach(() => {
+          wallet = new Wallet();
+          transaction = wallet.createTransaction({ recipient: 'foo-address', amount: 65 });
+          rewardTransaction = Transaction.rewardTransaction({ address: wallet.publicKey });
+        });
+        describe('and the transaction data is valid', () => {
+          it('returns true', () => {
+            newChain.addBlock({ data: [transaction, rewardTransaction] });
+            expect(blockchain.validTransactionData({ chain: newChain.chain })).toBe(true);
+          });
+        });
+        describe('and the transaction data has multiple rewards', () => {
+          it('returns false', () => {
+            newChain.addBlock({ data: [transaction, rewardTransaction, rewardTransaction] });
+            expect(blockchain.validTransactionData({ chain: newChain.chain })).toBe(false);
+          });
+        });
+        describe('and the transaction data has at least one malformed outputMap', () => {
+          describe('and the transaction is not a reward transaction', () => {
+            it('returns false', () => {
+              transaction.outputMap[wallet.publicKey] = 999999;
+              newChain.addBlock({ data: [transaction, rewardTransaction ]});
+              expect(blockchain.validTransactionData({ chain: newChain.chain })).toBe(false);
+            });
+          });
+          derscribe('and the transaction is a reward transaciton', () => {
+            it('returns false', () => {});
+          });
+        });
+        describe('and the transaction data has at least one malformed input', () => {
+          it('returns false', () => {});
+        });
+        describe('and a block contains multiple identical transactions', () => {
+          it('returns false', () => {});
+        });
+      });
     });
